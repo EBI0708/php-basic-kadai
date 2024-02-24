98172000babfaad09f5f64dca0294fdb9327c72f
EBI0708 <150600357+EBI0708@users.noreply.github.com>
2024-02-24 16:15:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_007</title>
+</head>
+<body>
+    <P>
+        <?php
+             $personal_data = ['name' => 'onion', 'price' => 200, 'weught' => '160'];
+ 
+             print_r($personal_data);
+             
+             ?>
+         </p>
+    
+</body>
+</html>
